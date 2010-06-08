27175b320d7ebf35998c8198e9f49ffb85a26608
Fernando Guillen <fguillen.mail@gmail.com>
2010-06-08 20:50:54 +0200

    rdiscount markdown interpreter

--- config/environment.rb
+++ config/environment.rb
@@ -25,6 +25,8 @@ Rails::Initializer.run do |config|
   config.gem "activemerchant", :lib => 'active_merchant', :version => '1.5.1'
   config.gem "factory_girl", :source => "http://gemcutter.org"
   config.gem "ruby_regex"
+  config.gem "rdiscount", :source => 'http://gemcutter.org'
+
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
